9d6ce7758dcf15ced4cb76bc97fa0863ff346e34
Rahul Bhatia <Superhuman07@users.noreply.github.com>
2016-09-11 23:11:31 +0530

    Add Comments

--- app/src/main/java/com/example/admin/moviesapp/MovieItem.java
+++ app/src/main/java/com/example/admin/moviesapp/MovieItem.java
@@ -3,6 +3,8 @@ package com.example.admin.moviesapp;
 /**
  * Created by Admin on 02-02-2016.
  */
+ 
+// Movie item to store and get all the movie details from the moviedb api
 public class MovieItem {
     String id;
     String title;
@@ -12,6 +14,8 @@ public class MovieItem {
     Double voteAverage;
     Double popularity;
 
+
+// Constructor for the class MovieItem
     public MovieItem(String movieId,  String movieTitle, String moviePosterPath, String movieOverview, String movieReleaseDate, Double movieVoteAverage, Double moviePopularity) {
         id = movieId;
         title = movieTitle;
