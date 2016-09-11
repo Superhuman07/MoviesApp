90453dd5e5de544c0160f6dfd626d11d0b3d6c2b
Rahul Bhatia <Superhuman07@users.noreply.github.com>
2016-09-11 23:07:16 +0530

    Add Comments

--- app/src/main/java/com/example/admin/moviesapp/IsOnline.java
+++ app/src/main/java/com/example/admin/moviesapp/IsOnline.java
@@ -7,12 +7,19 @@ import android.net.NetworkInfo;
 /**
  * Created by Admin on 05-02-2016.
  */
+ 
+ // Check whether the user is online or not
 public class IsOnline {
     public static boolean isNetworkStatusAvailable (Context context) {
+        
+        //Get connectivity manager system service to get network information
         ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
         if (connectivityManager != null)
         {
+            // Get the network information from above the connectivity manager 
             NetworkInfo netInfos = connectivityManager.getActiveNetworkInfo();
+            
+            // Gives true if the user is connected to a internet network
             if(netInfos != null)
             {
                 return netInfos.isConnected();
