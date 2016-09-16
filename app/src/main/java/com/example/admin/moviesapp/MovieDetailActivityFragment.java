1c41642b5254e0ead4dc20778d18de0a286b9000
Rahul Bhatia <Superhuman07@users.noreply.github.com>
2016-09-16 23:58:59 +0530

    Add Comments

--- app/src/main/java/com/example/admin/moviesapp/MovieDetailActivityFragment.java
+++ app/src/main/java/com/example/admin/moviesapp/MovieDetailActivityFragment.java
@@ -22,17 +22,26 @@ public class MovieDetailActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+                                 
+        // Getting the intent send by the main activity                         
         Intent intent = getActivity().getIntent();
+        // Image path that will give the poster for movie detail of w185
         String imagePath = "http://image.tmdb.org/t/p/w185";
         View rootView = inflater.inflate(R.layout.fragment_movie_detail, container, false);
         ImageView posterView = (ImageView) rootView.findViewById(R.id.poster);
+        
+        // Setting the movie poster image on the poster view
         Picasso.with(getActivity()).load(imagePath + intent.getExtras().get("posterPath")).into(posterView);
+        // Setting title of the movies
         ((TextView) rootView.findViewById(R.id.title))
                 .setText(intent.getStringExtra("title"));
+        // Setting overview of the movies
         ((TextView) rootView.findViewById(R.id.overview))
                 .setText(intent.getStringExtra("overview"));
+        // Setting rating of the movies        
         ((TextView) rootView.findViewById(R.id.rating))
                 .setText("Rating: " + ((Double) intent.getDoubleExtra("voteAverage", 0.0)).toString());
+        // Setting release date of the movies        
         ((TextView) rootView.findViewById(R.id.release_date))
                 .setText("Released on " + intent.getStringExtra("releaseDate"));
         return rootView;
